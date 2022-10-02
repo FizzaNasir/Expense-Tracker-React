839fca99b2e597ec6c33775c03c7e87dec7777d5
Fizza <fizzanasir976@gmail.com>
2022-10-02 11:44:02 +0500

    static data

--- src/App.js
+++ src/App.js
@@ -2,4 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Grid} from '@material-ui/core';
 import Detail from "./Components/Details/Detail"
-export de
+import useStyles from './styles';
+export default function App(){
+    const classes=useStyles();
+    return(
+        <div>
+            <Grid className={classes.grid} container spacing={0} justify='center' alignItems='center' style={{height: '100vh'}}>
+                <Grid items xm={12} sm={4}>
+                    <Detail title="Income"/>
+                </Grid>
+                <Grid items xm={12} sm={4}>
+                     Main
+                </Grid>
+                <Grid items xm={12} sm={4}>
+                    <Detail title="Expense"/>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
---
+++ src/Components/Details/Detail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Card, CardHeader, CardContent, Typography} from '@material-ui/core';
+import useStyles from './DetailStyles';
+export default function Detail(props){ //we can also destructure the prop({title})
+    const classes= useStyles();  //cz makestyles returns a hook. you can use any other name instead of useStyles
+    return(
+        <div>
+            <Card className={props.title=="Income"? classes.income: classes.expense}>
+                <CardHeader title={props.title}/>
+                <CardContent>
+                    <Typography>
+                        $50
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
